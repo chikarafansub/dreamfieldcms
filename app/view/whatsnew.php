effd15ad8b1223370d5edfc5984735a76ded6c6c
hemoloyag <nesdelisi@yandex.com.tr>
2020-05-29 11:25:25 +0300

    Unutulan sayfa
    Unutu

---
+++ app/view/whatsnew.php
@@ -0,0 +1,25 @@
+<div class="container">
+<div class="row">
+<div class="col-ms-12">
+<h3>Neler Yeni?</h3>
+<h4>v1.10</h4>
+<ul>
+<li>Sözlük sayfası eklendi.</li>
+<li>Footer'a sürüm geçmişine giden bağlantı eklendi.</li>
+<li>Footer'a sözlük sayfasına giden bağlantı eklendi.</li>
+</ul>
+<br />
+<h4>v1.02</h4>
+<ul>
+<li>teamclass.php'deki hatalı yer göstermesinden dolayı verileri kaydırma hatası giderildi.</li>
+<li>Footer'a sürüm bilgisi eklendi.</li>
+</ul>
+<br />
+<h4>v1.01</h4>
+<ul>
+<li>viewfootballer.php'deki gösterme hatası giderildi.</li>
+<li>createteam.php'deki göndermeme hatası giderildi.</li>
+</ul>
+</div>
+</div>
+</div>
